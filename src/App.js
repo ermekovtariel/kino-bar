348aa4b4d76b3c2a2e9786f5d5b702a710005944
ermekovtariel <ermekovtariel@gmail.com>
2021-03-26 11:50:36 +0600

    A

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from "react";
 import "./App.css";
-import Row from './Row';
-import Banner from './Banner';
-import requests from './requests';
-import Nav from './Nav';
+
+import { Route } from "react-router";
+import Auth from "./Auth";
+import OpenedCinema from "./OpenedCinema";
 
 function App() {
   return (
     <div className="app">
-      <Nav />
-      <Banner />
-      <Row  
-            title='NETFLIX Originals' 
-            fetchUrl={requests.fetchNewflixOriginals} 
-            isLargeRow
-      />
-      <Row  title='Top Rated' fetchUrl={requests.fetchTopRated} />
-      <Row  title='Action Movies' fetchUrl={requests.fetchActionsMovies} />
-      <Row  title='Comedt Movies' fetchUrl={requests.fetchComedyMovies} />
-      <Row  title='Horror Movies' fetchUrl={requests.fetchHorrorMovies} />
-      <Row  title='Romance Movies' fetchUrl={requests.fetchRomanceMovies} />
-      <Row  title='Documentaries' fetchUrl={requests.fetchDocumentaries} />
+      <Route path='/' exact>
+          <OpenedCinema />
+      </Route>
+      <Route path='/auth' exact>
+          <Auth />
+      </Route>
     </div>
   );
 }
--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import "./Nav.css";
 import Logo from "./images.png";
+import { Link, Route } from 'react-router-dom';
 
 function Nav() {
     
@@ -13,24 +14,26 @@ function Nav() {
                 handleShow(true)
             }else handleShow(false)
         })
-        return ()=>{
-            window.removeEventListener("scroll")
-        }
+
     }, [])
 
     return (
         <div className={`nav ${show && "nav__black"}`}>
-            <img 
+            <Link to='/'>
+                <img 
                 className='nav___logo'
                 src={Logo}
                 alt='Kino Bar'
-            />
+                />
+            </Link>
 
+        <Link to='/auth'>
             <img 
                 className='nav__avatar' 
                 src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-45gYPtG2C5jtj3zGW-T5-GsTlCl4m4jhVAlP2iA0SrFfRZg1S1hm-Iv6E2XRvNPAVuo&usqp=CAU'
                 alt='Log'
             />
+        </Link>
         </div>
     )
 }
